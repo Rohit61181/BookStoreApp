5494b1eb3b1ba809e9e30b3ea95bd72696aefc1a
Rohit <rohitchowdhary20122002@gmail.com>
2024-04-21 10:46:12 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import list from "../../public/list.json";
+import Cards from "./Cards";
+
+function Freebook() {
+const filterData = list.filter((data) => data.category === "Free");
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          reprehenderit quia, cumque, officiis aperiam pariatur molestias
+          architecto omnis accusantium, dicta dolore culpa reiciendis nisi error
+          sint iste iusto? Dolorem ipsam magni fuga amet illo voluptatum natus
+          saepe voluptatem, molestias veritatis quibusdam a voluptates illum
+          distinctio, possimus sapiente repellendus, incidunt optio!
+        </p>
+        </div>
+      
+      <div>
+        <Slider {...settings}>
+          {filterData.map((item) => (
+            <Cards item = {item } key = {item.id} />
+              
+          ))}
+        </Slider>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
